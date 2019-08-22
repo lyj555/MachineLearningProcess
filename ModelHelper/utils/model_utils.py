20b628e3d6ba87ca5554648c1a3db37cf2604045
liuyongjie05 <liuyongjie05@58.com>
2019-08-22 21:16:01 +0800

    update

--- ModelHelper/utils/model_utils.py
+++ ModelHelper/utils/model_utils.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 
-def cross_validation_score(train, y, k_fold, model, **kwargs):
+
+def cross_validation_score(train, y, k_fold, model, model_param=None, return_model_train_time=False, **kwargs):
+    t1 = datetime.now()
     from sklearn.model_selection import cross_val_score
 
+    if model_param is not None:
+        model = model.set_params(**model_param)
     score_list = cross_val_score(estimator=model, X=train, y=y, cv=k_fold, **kwargs)
-    return sum(score_list)/len(score_list)
+    t2 = datetime.now()
+    if return_model_train_time:
+        return sum(score_list) / len(score_list), (t2 - t1).seconds
+    else:
+        return sum(score_list) / len(score_list)
 
 
-def valid_set_score(train_x, train_y, valid_x, valid_y, model, metric_func, **kwargs):
-    clf = model.fit(train_x, train_y, **kwargs)
-    valid_pred = clf.predict_proba(valid_x)[:,1]
-    return metric_func(valid_y, valid_pred)
+def valid_set_score(train_x, train_y, valid_x, valid_y, model, metric_func, model_param=None,
+                    return_model_train_time=False):
+    t1 = datetime.now()
+    if model_param is not None:
+        model = model.set_params(**model_param)
+    clf = model.fit(train_x, train_y)
+    valid_pred = clf.predict_proba(valid_x)[:, 1]
+    t2 = datetime.now()
+    if return_model_train_time:
+        return metric_func(valid_y, valid_pred), (t2 - t1).seconds
+    else:
+        return metric_func(valid_y, valid_pred)
