cacd7474efb468e6940cb7291a10b3fff4252b17
刘勇杰 <liuyongjie05@58.com>
2020-07-09 23:30:30 +0800

    update ModelHelper

--- ModelHelper/model_helper/__init__.py
+++ ModelHelper/model_helper/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from . import feature_selection, hyper_parameter_tuning, model_ensemble, model_explain
-
-__all__ = ["feature_selection", "hyper_parameter_tuning", "model_ensemble", "model_explain"]
+# from . import feature_selection, hyper_parameter_tuning, model_ensemble, model_explain
+#
+# __all__ = ["feature_selection", "hyper_parameter_tuning", "model_ensemble", "model_explain"]
 __version__ = "0.2.0"
--- ModelHelper/setup.py
+++ ModelHelper/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='model-helper',
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com/lyj555/MachineLearningProcess',
     packages=find_packages(),
     author='liuyongjie',
@@ -12,10 +12,10 @@ setup(
     description='Model Helper',
     long_description='A Python implementation of model helper, with feature selection, hyper-parameter tune and feature engineering',
     # download_url='https://github.com/fmfn/BayesianOptimization/tarball/0.6',
-    install_requires=[
-        "pandas >= 0.23.0",
-        "numpy >= 1.10.0",
-        "scikit-learn >= 0.19.0",
-        "shap == 0.30.0"
-    ],
+    # install_requires=[
+    #     "pandas >= 0.23.0",
+    #     "numpy >= 1.10.0",
+    #     "scikit-learn >= 0.19.0",
+    #     "shap == 0.30.0"
+    # ],
 )
